ca7139a1f0c6d68d0605808aec6016c225de3636
tianyis <sangtianyi01@gmail.com>
2021-01-21 18:10:28 -0800

    add openConversation logic

--- src/components/dashBoard.js
+++ src/components/dashBoard.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import OpenConversation from './openConversation'
 import SideBar from './sideBar'
+import { useConversations } from '../contexts/conversationsProvider'
 
 export default function DashBoard({id}) {
+
+    const { selectedConversation } = useConversations()
+
     return (
         <div className="d-flex" style={{height: '100vh'}}>
             <SideBar id={id}/>
+            {selectedConversation && <OpenConversation />}
         </div>
     )
 }
